e303fb2946bb71d0041eb5a1aa791f3375ffa403
ramakrishnan9 <49511467+ramakrishnan9@users.noreply.github.com>
2019-04-24 17:13:48 +0530

    p.c

---
+++ p.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ main()
+ {
+  char n;
+  clrscr();
+  printf("Enter the first letter":);
+  scanf('%c',&n);
+  if((n==a)&&(n==e)&&(n==i)&&(n==o)&&(n==u))
+   {
+    printf("The Vowels is:",n);
+    }
+    else
+     {
+     printf("The Constant is:",n);
+     }
+     return 0;
+   }
