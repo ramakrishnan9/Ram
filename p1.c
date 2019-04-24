625618434f7a4826bb99491e04c52d1e0f2d4c30
ramakrishnan9 <49511467+ramakrishnan9@users.noreply.github.com>
2019-04-24 17:28:53 +0530

    p1.c

---
+++ p1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+ {
+  char a[10]={'a','b','c','d','e'};
+  printf("Enter the Alphabet:");
+   scanf('%c',&a[i]);
+   if(a[i]==a[0])
+      {
+      printf("The Alphet letter is:",a[i]);
+      }
+      else
+      {
+      printf("This is not Alphabet:",a[i]);
+      }
+      return 0;
+    }  
