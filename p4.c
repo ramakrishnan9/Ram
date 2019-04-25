c2d19e4620cf95495c9f7f225fdb0746eed79a4e
ramakrishnan9 <49511467+ramakrishnan9@users.noreply.github.com>
2019-04-25 17:33:17 +0530

    p4.c

--- p4.c
+++ p4.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
  {
- int i,n;
+ int i,n,a[20];
  printf("Enter the numbers:");
  scanf("%d",&n);
- for(i=1;i<=n;i++)
+ for(i=0;i<=n;i++)
   {
+  	scanf("%d",&a[i]);
+  }
    printf("The total number is:%d",n);
-   }
 return 0;
 }
