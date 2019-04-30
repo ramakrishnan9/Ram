f978cc9f47ceee08e78bc384c55ac389dcc8130c
ramakrishnan9 <49511467+ramakrishnan9@users.noreply.github.com>
2019-04-30 09:27:00 +0530

    p8.c

---
+++ p8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+ {
+  int
+  printf("Enter the number:");
+  scanf("%d",&n);
+  if(n<=1000)
+   {
+   printf("This is Palindrome number:%d",n);
+   }
+   else
+   {
+   printf("This is not a Palindrome number:%d",n);
+   }
+   return 0;
+   }
