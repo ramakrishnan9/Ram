2db8f268d5e7dfd9291b9de2344ae028c8c39a8b
ramakrishnan9 <49511467+ramakrishnan9@users.noreply.github.com>
2019-05-14 10:37:16 +0530

    p12.py

---
+++ p12.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the number:"))
+if n>1:
+for i in range(2,n):
+if(n%i)==0:
+print(n,"is not a prime number")
+print(i,"times",num//i,"is",num)
+break
+elif:
+print(n,"is a prime number")
+else:
+print(n,"is not a prime number")
