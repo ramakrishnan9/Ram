449ea60fece464f563c6d1668fcc6e9795bfc55e
ramakrishnan9 <49511467+ramakrishnan9@users.noreply.github.com>
2019-05-14 13:40:15 +0530

    p15.c

---
+++ p15.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+ {
+  int n,re,sum=0;
+  scanf("%d",&n);
+  while(n>=0)
+   {
+   n=n%10;
+   re=n*n*n;
+   sum=sum+re;
+   n=n/10;
+   }
+   printf("The Amstrong Number is:%d",sum);
+  }  
