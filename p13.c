7f86a650357b3ef55190faedf310dbb9cf5ce19e
ramakrishnan9 <49511467+ramakrishnan9@users.noreply.github.com>
2019-05-14 12:03:16 +0530

    p13.c

---
+++ p13.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+ {
+  int n,m;
+  scanf("%d%d",&n,&m);
+  k=0;
+  while(n%m==0)
+  {   
+    printf("The Even number is:%d",n);
+    k++;
+    }
+    return 0;
+    }
