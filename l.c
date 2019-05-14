0ef01b17f92be879b8309979764375eb7148a9f1
ramakrishnan9 <49511467+ramakrishnan9@users.noreply.github.com>
2019-05-14 11:44:36 +0530

    l.c

---
+++ l.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+ {
+  int i,n;
+  printf("Enter the number:");
+  scanf("%d",&n);
+  for(i=10;i<=n;i++)
+   {
+  if(i%2==1)
+    {
+     printf("The odd number is:%d",i);
+     }
+    }
+    retunr 0;
+    }
