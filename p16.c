8af0fe16699eed7af2b63148377e5a31e2f5bc4a
ramakrishnan9 <49511467+ramakrishnan9@users.noreply.github.com>
2019-05-20 17:19:27 +0530

    p16.c

--- p16.c
+++ p16.c
@@ -3,15 +3,15 @@ int main()
  {
  int n,check,rem,sum=0,i,m;
  scanf("%d%d",&n,&m);
- for(i=n;i<=m;i++)
- {
-  if(n!=m)
+ i=n;
+ while(n!=m)
  {
     rem=i%10;
   sum=sum+(rem*rem*rem);
   check=i/10;
-  }
+  } 
   printf(sum);
+ i++;
  }
     return 0;
  }
