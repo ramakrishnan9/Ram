d1b03e33c03f944d33fa8029c318aa3a0d93f241
ramakrishnan9 <49511467+ramakrishnan9@users.noreply.github.com>
2019-07-20 14:58:32 +0530

    amd.c

--- amd.c
+++ amd.c
@@ -1,24 +1,12 @@
 #include<stdio.h>
 int main()
+{
+  int i,n,p;
+   scanf("%d",&n);
+ for(i=1;i<=n;i++)
  {
- int sum=0,n,i,j;
- printf("Enter the number:");
- scanf("%d%d",&n1,&n2);
- for(i=0;i<=n;i++)
-  {
-   for(j=0;j<=n2;j++)
-     {
-      if(n1<n2!=0)
-       {
-       n1=n%10;
-       sum=sum+(n1*n1*n1);
-       n=n/10;
-       }
-      }  
-       if(sum==n)      
-       {
-       printf("The amstrong number is:%d",sum)
-       }
-     }
-     return 0;
-     }
+  p=n*i;
+ printf("%d\n",p);
+ }
+ return 0;
+}
